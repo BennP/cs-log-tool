4aa244a731e7c7524c044fcbfd8ddfb81bc7a594
Benn Porscke <bennporscke@gmail.com>
2015-12-17 13:17:03 +0100

    Some colour changes  Added client interaction lines in the GWF and Location pages  Changed to Python 3

--- log-tool-v2.py
+++ log-tool-v2.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# -*- coding: iso-8859-1 -*-
 import argparse
 import re
 import os
@@ -28,7 +29,6 @@ import os
 #          - Use CSS to reduce html size
 #
 # --------------------------------------
-
 
 # Argument parser
 parser = argparse.ArgumentParser(description='Process a logfile from Astro.')
@@ -62,6 +62,7 @@ gwf_ignore_list = []
 loc_search_list = []
 loc_search_conf_list = []
 loc_search_color_conf_list = []
+loc_ignore_list = []
 
 #-----------------
 # BoxCalc trace lists
@@ -81,8 +82,8 @@ syspar_txt = 'System parameter not defined'
 # Classes
 
 # Row class
-class LogRow:
-   'Common base class for a line of trace data'
+class LogRow(object):
+   """Common base class for a line of trace data"""
    tot_row = 0
 
    def __init__(self, row_id, time_stamp, row_text, row_type):
@@ -92,11 +93,6 @@ class LogRow:
         self.row_type = row_type
         LogRow.tot_row += 1
 
-   def displayRowTimeStamp(self):
-        print "Time " + self.time_stamp
-
-   def displayRowText(self):
-        print "- %s" % self.row_text
 
 # GWF Row class
 class GWFRow:
@@ -227,6 +223,15 @@ class LocationSearchConf:
         self.color_text = color_text
         LocationSearchConf.tot_row += 1
 
+# Location Search ignore class
+class LocSearchIgnore:
+   """Common base class for Location Search Ignore config data"""
+   tot_row = 0
+
+   def __init__(self, ident_text):
+        self.ident_text = ident_text
+        LocSearchIgnore.tot_row += 1
+
 # Box Calc  class
 class BoxCalc:
    'Common base class for BoxCalc config data'
@@ -269,8 +274,8 @@ def get_row_type(text):
     """Determine row type from text."""
     for index in range(len(trace_class_list)):
         if text.find(trace_class_list[index].ident_text) != -1:
-            return index;
-    return -1;
+            return index
+    return -1
 
 def get_row_class(id):
     """Get row class """
@@ -282,8 +287,8 @@ def get_gwf_row_type(text):
     """Determine gwf row type from text."""
     for index in range(len(gwf_conf_list)):
         if text.find(gwf_conf_list[index].ident_text) != -1:
-            return index;
-    return -1;
+            return index
+    return -1
 
 def get_gwf_row_color(id):
     """Get gwf row color """
@@ -298,8 +303,8 @@ def limit_trace(text):
     else:
         for index in range(len(trace_ignore_list)):
             if text.find(trace_ignore_list[index].ident_text) != -1:
-                return True;
-    return False;
+                return True
+    return False
 
 def limit_gwf_trace(text):
     """Determine gwf row that should be ignored."""
@@ -308,30 +313,30 @@ def limit_gwf_trace(text):
     else:
         for index in range(len(gwf_ignore_list)):
             if text.find(gwf_ignore_list[index].ident_text) != -1:
-                return True;
-    return False;
+                return True
+    return False
 
 def is_gwf_trace(text):
     """Determine if text is gwf trace that should be included."""
     for index in range(len(gwf_select_conf_list)):
         if text.find(gwf_select_conf_list[index].ident_text) != -1:
-            return True;
-    return False;
+            return True
+    return False
 
 
 def is_loc_search_trace(text):
     """Determine if text is location search that should be included."""
     for index in range(len(loc_search_conf_list)):
         if text.find(loc_search_conf_list[index].ident_text) != -1:
-            return True;
-    return False;
+            return True
+    return False
 
 def get_loc_search_row_type(text):
     """Determine Location Search row type from text."""
     for index in range(len(loc_search_color_conf_list)):
         if text.find(loc_search_color_conf_list[index].ident_text) != -1:
-            return index;
-    return -1;
+            return index
+    return -1
 
 def get_loc_search_row_color(id):
     """Get Location Search row color """
@@ -339,19 +344,29 @@ def get_loc_search_row_color(id):
         return '"black"'
     return '"' + loc_search_color_conf_list[id].color_text + '"'
 
+def limit_loacsearch_trace(text):
+    """Determine location search row that should be ignored."""
+    if args.show_all:
+        return False
+    else:
+        for index in range(len(loc_ignore_list)):
+            if text.find(loc_ignore_list[index].ident_text) != -1:
+                return True
+    return False
+
 def is_box_calc_trace(text):
     """Determine if text is Box Calc that should be included."""
     for index in range(len(box_calc_conf_list)):
         if text.find(box_calc_conf_list[index].ident_text) != -1:
-            return True;
-    return False;
+            return True
+    return False
 
 def get_box_calc_row_type(text):
     """Determine Box Calc row type from text."""
     for index in range(len(box_calc_color_conf_list)):
         if text.find(box_calc_color_conf_list[index].ident_text) != -1:
-            return index;
-    return -1;
+            return index
+    return -1
 
 def get_box_calc_row_color(id):
     """Get Box Calc row color """
@@ -464,19 +479,19 @@ def do_code_indent(text):
     """ If the line is a "APPL-OK   : :      :  X:" line then do a indention according to the level given at x. """
     """ And do some rearanging of the actual test to make it more readable                                      """
     if text.find('APPL-OK   : :      :') != -1:
-        offset = text.find('OK   : :      :');
+        offset = text.find('OK   : :      :')
         if offset == -1:
-            print ' This line must be wrong'
-            print text
+            print(' This line must be wrong')
+            print (text)
             return text
         else:
             num = text[(offset + 16):(offset + 20)].split(':',2)
             indent_number = int (num[0])
             indent_str = '                                                              '
-            offset_tail = text.find('./');
+            offset_tail = text.find('./')
             if offset == -1:
-                print ' This line must be wrong'
-                print text
+                print(' This line must be wrong')
+                print (text)
                 return text
             # Now we have indented the text. Lets move some stuf around to make it mor readable
             # split this into several parts /ikea/lpp/astrodev/cosmos2/src/ObjL/ObjL54T1.c:3816,Assignment.SetUnderProcessing, Caller: /ikea/lpp/astrodev/cosmos2/src/ObjL/ObjL47T1.c:2223, RetOk
@@ -568,15 +583,25 @@ def get_config_path():
     env_parts = env_path.split(':')
     for dir in env_parts:
         if os.path.isfile(dir + '/trace-color.conf'):
-            print 'Found the file ' + dir
+            print ('Found the file ' + dir)
             return dir + '/'
     return ''
 
-
-
+#def parse_file( debug_file, output_name, output_dir):
+#    """
+#    Will pars a given Astro log file
+#
+#    Will produce output HTML files
+#      - Main file with
+#        - Info on the whole file
+#        - Links to the specific sub files
+#        - Entier log colour coded and cleaned
+#    """
+#
 #----------------------
 #  Main starts
 #----------------------
+
 args = parser.parse_args()
 
 trace_file_full_name = str(args.html_log_file_dir) + '/' + str(args.html_log_file_name) + '.html'
@@ -588,13 +613,13 @@ trace_file_name = str(args.html_log_file_name) + '.html'
 gwf_trace_file_name = 'GWF-' + str(args.html_log_file_name) + '.html'
 loc_search_trace_file_name = 'LocSearch-' + str(args.html_log_file_name) + '.html'
 box_calc_trace_file_name = 'BoxCalc-' + str(args.html_log_file_name) + '.html'
-print 'Astro log file = ' + args.log_file_name
-print 'Output file    = ' + trace_file_full_name
-print 'Output file GWF  = ' + gwf_trace_file_full_name
-print 'Output file LocSearch  = ' + loc_search_trace_file_full_name
-print 'Output file BoxCalc  = ' + box_calc_trace_file_full_name
+print ('Astro log file = ' + args.log_file_name)
+print ('Output file    = ' + trace_file_full_name)
+print ('Output file GWF  = ' + gwf_trace_file_full_name)
+print ('Output file LocSearch  = ' + loc_search_trace_file_full_name)
+print ('Output file BoxCalc  = ' + box_calc_trace_file_full_name)
 
-log_file    = open(args.log_file_name)
+log_file    = open(file=args.log_file_name,encoding='iso-8859-1')
 output_file = open(trace_file_full_name, 'w')
 gwf_output_file = open(gwf_trace_file_full_name, 'w')
 loc_search_output_file = open(loc_search_trace_file_full_name, 'w')
@@ -614,7 +639,7 @@ try:
         gwf_conf_list.append(add_row)
     gwf_config_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'gwf-color.conf'
+    print ("Error: can\'t find file or read data " + 'gwf-color.conf')
     raise
 
 try:
@@ -626,7 +651,7 @@ try:
         trace_class_list.append(add_row)
     trace_config_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'trace-color.conf'
+    print ("Error: can\'t find file or read data " + 'trace-color.conf')
     raise
 
 try:
@@ -638,7 +663,7 @@ try:
         loc_search_color_conf_list.append(add_row)
     loc_search_color_config_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'loc-search-color.conf'
+    print ("Error: can\'t find file or read data " + 'loc-search-color.conf')
     raise
 
 try:
@@ -650,7 +675,7 @@ try:
         box_calc_color_conf_list.append(add_row)
     box_calc_color_config_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'box-calc-color.conf'
+    print ("Error: can\'t find file or read data " + 'box-calc-color.conf')
     raise
 
 #-Get ignore lists -------------
@@ -661,7 +686,17 @@ try:
         gwf_ignore_list.append(add_row)
     gwf_ignore_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'gwf-ignore.conf'
+    print ("Error: can\'t find file or read data " + 'gwf-ignore.conf')
+    raise
+
+try:
+    loc_ignore_file    = open(config_path + 'loc-search-ignore.conf')
+    for ignore_line in loc_ignore_file:
+        add_row = LocSearchIgnore(ignore_line.rstrip())
+        loc_ignore_list.append(add_row)
+    loc_ignore_file.close()
+except IOError:
+    print ("Error: can\'t find file or read data " + '-ignore.conf')
     raise
 
 try:
@@ -671,18 +706,18 @@ try:
         trace_ignore_list.append(add_row)
     trace_ignore_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'trace-ignore.conf'
+    print ("Error: can\'t find file or read data " + 'trace-ignore.conf')
     raise
 
 #-Get gwf select lists -------------
 try:
     gwf_select_file    = open(config_path + 'gwf-select.conf')
     for gwf_select_line in gwf_select_file:
-        add_row = LocationSearch(gwf_select_line.rstrip())
+        add_row = GWFSelect(gwf_select_line.rstrip())
         gwf_select_conf_list.append(add_row)
     gwf_select_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'gwf-select.conf'
+    print ("Error: can\'t find file or read data " + 'gwf-select.conf')
     raise
 
 #-Get location search lists -------------
@@ -693,7 +728,7 @@ try:
         loc_search_conf_list.append(add_row)
     loc_search_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'loc-search.conf'
+    print ("Error: can\'t find file or read data " + 'loc-search.conf')
     raise
 
 #-Get Box Calc lists -------------
@@ -704,7 +739,7 @@ try:
         box_calc_conf_list.append(add_row)
     box_calc_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'box-calc.conf'
+    print ("Error: can\'t find file or read data " + 'box-calc.conf')
     raise
 
 #-Get css text -------------
@@ -715,7 +750,7 @@ try:
         css_text.append(css_line)
     css_text_file.close()
 except IOError:
-    print "Error: can\'t find file or read data " + 'css.conf'
+    print ("Error: can\'t find file or read data " + 'css.conf')
     raise
 
 #------------------------------
@@ -729,7 +764,7 @@ for line in log_file:
         do_set_wrapped_mark(line_id, line)
         matchObj = re.match( r'[0-9]{2}-[0-9]{2}-[0-9]{2}', line, re.M|re.I)
         if matchObj:
-            offset = line.find("Real=");
+            offset = line.find("Real=")
             if offset == -1:
                 add_row = LogRow(line_id,line[0:17],' ' + line[(19):],get_row_type(line))
             else:
@@ -748,7 +783,7 @@ for line in log_file:
                 line_id = line_id + 1
 
 log_file.close()
-log_file    = open(args.log_file_name)
+log_file    = open(file=args.log_file_name, encoding='iso-8859-1')
 
 #------------------------------
 # Rip the logs apart and find gwf data
@@ -766,7 +801,7 @@ for line in log_file:
                 line_id = line_id + 1
 
 log_file.close()
-log_file    = open(args.log_file_name)
+log_file    = open(file=args.log_file_name, encoding='iso-8859-1')
 
 #------------------------------
 # Rip the logs apart and find Location Search data
@@ -776,14 +811,15 @@ line_id = 0
 for line in log_file:
     matchObj = re.match( r'[0-9]{2}-[0-9]{2}-[0-9]{2}', line, re.M|re.I)
     if matchObj:
+        offset = line.find("Real=");
         if is_loc_search_trace(line):
-            offset = line.find("Real=");
-            add_row = LocSearchRow(line_id,line[0:17],line[(offset + 21):],get_loc_search_row_type(line))
-            loc_search_list.append(add_row)
-            line_id = line_id + 1
+            if not limit_loacsearch_trace(line):
+                add_row = LocSearchRow(line_id,line[0:17],line[(offset + 21):],get_loc_search_row_type(line))
+                loc_search_list.append(add_row)
+                line_id = line_id + 1
 
 log_file.close()
-log_file    = open(args.log_file_name)
+log_file    = open(file=args.log_file_name, encoding='iso-8859-1')
 #------------------------------
 # Rip the logs apart and find Box Calc data
 # The ripped log will later be used to generate a separat HTML page for Box Calc data.
@@ -801,16 +837,16 @@ for line in log_file:
 #---------------
 # Result of file runtrough
 #---------------
-print "Number of DB Errors = %d" % DbError.error_count
-print "Number of APPL-ERROR = %d" % ApplError.appl_count
-print "Number of CallTrace = %d" % CallTrace.trace_count
-print "Number of SysPar errors = %d" % SysParTrace.syspar_count
-print "Total rows %d" % LogRow.tot_row
-print "Total GWF rows %d" % GWFRow.tot_row
-print "Total Location Search rows %d" % LocSearchRow.tot_row
-print "Total Box Calc rows %d" % BoxCalcRow.tot_row
-print "End mark %d" % end_line
-print "Wrapped mark %d" % wrapped_line
+print ("Number of DB Errors = %d" % DbError.error_count)
+print ("Number of APPL-ERROR = %d" % ApplError.appl_count)
+print ("Number of CallTrace = %d" % CallTrace.trace_count)
+print ("Number of SysPar errors = %d" % SysParTrace.syspar_count)
+print ("Total rows %d" % LogRow.tot_row)
+print ("Total GWF rows %d" % GWFRow.tot_row)
+print ("Total Location Search rows %d" % LocSearchRow.tot_row)
+print ("Total Box Calc rows %d" % BoxCalcRow.tot_row)
+print ("End mark %d" % end_line)
+print ("Wrapped mark %d" % wrapped_line)
 
 #---------------
 # Start making the HTML files
